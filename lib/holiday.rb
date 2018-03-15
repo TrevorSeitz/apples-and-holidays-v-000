fbf6a900f5bc137343bf49d85984ad6f426a81b2
Trevor Seitz <trevinla@gmail.com>
2018-03-15 15:14:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,8 +32,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # hash3 = hash1[:christmas]
   # hash2 << supply
   # hash3 << supply
-  hash1.each do |holiday, supply|
-    holiday << supply
+  hash1.each do |holiday|
+    hash1[holiday] << supply
   end
 end
 
